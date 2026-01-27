38ae910261b9f2c6fa3008b449259de07edcd5b8
gdgeek <dirui1@yeah.net>
2026-01-27 10:59:24 +0800

    修复: 迁移文件列名改为log_value

--- src/models/migrations/005_change_value_to_text.sql
+++ src/models/migrations/005_change_value_to_text.sql
@@ -1,4 +1,4 @@
--- Migration: Change value column from JSON to TEXT
--- This migration converts the value column from JSON type to TEXT type
+-- Migration: Change log_value column from JSON to TEXT
+-- This migration converts the log_value column from JSON type to TEXT type
 
-ALTER TABLE logs MODIFY COLUMN value TEXT NOT NULL;
+ALTER TABLE logs MODIFY COLUMN log_value TEXT NOT NULL;
